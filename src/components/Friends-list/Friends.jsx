bb46a67419c93e39bf3c22819b37e09f52c710f8
ZakZanko <zakzanko2112@gmail.com>
2023-06-30 17:21:38 +0300

    fixed

---
+++ src/components/Friends-list/Friends.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import css from './Friends.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <div className={css.friends}>
+      <ul className={css.friends__list}>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+          <li className={css.friends__item} key={id}>
+            <span className={`${css.friends__status} ${css[isOnline]}`}>
+              {isOnline}
+            </span>
+            <img className={css.avatar} src={avatar} alt={name} />
+            <p className={css.friends__name}>{name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+FriendList.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+};
